2777a0037a90b0b83d1e9f014fcadc471c538fe1
erwin <erwin@NEHEMIA>
2016-11-04 09:20:57 +0100

    Make sure DCtype and subtype are selectable in popup window

--- collbank/collbank/collection/static/collection/scripts/collbank.js
+++ collbank/collbank/collection/static/collection/scripts/collbank.js
@@ -16,7 +16,10 @@ function type_change() {
   // Get the value of the selected [DCtype]
   var dctype_type = $('#id_DCtype').val();
   // Create the URL that is needed
-  var url_prefix = $(".container").attr("url_home");
+  var url_prefix = $(".container[url_home]").attr("url_home");
+  if (url_prefix === undefined) {
+    url_prefix = $("#container").attr("url_home");
+  }
   var sUrl = url_prefix + "subtype_choices/?dctype_type=" + dctype_type;
   // Make a request to get all subtype choices for this DCtype
   $.ajax({
